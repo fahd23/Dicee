24bb7d66ddad6d752ee7dc6e369aa7f39425a61c
Fahd <64308102+fahd23@users.noreply.github.com>
2021-10-21 15:47:58 +0530

    Refactor index.js

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-var randomNumber1 = Math.ceil(Math.random()*6);
-var randomImage = "images/dice"+randomNumber1+".png";
-document.querySelector("img").setAttribute("src",randomImage);
+const reloadBtn = document.querySelector("#reload");
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+const heading = document.querySelector("h1");
 
-var randomNumber2 = Math.ceil(Math.random()*6);
-var randomImage2 = "images/dice"+randomNumber2+".png";
-document.querySelector("img.img2").setAttribute("src",randomImage2);
-
-
-if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML="🚩 Player 1 wins!";
-}else if (randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML="Draw!";
-}else{
-  document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
+function generateRandomNumber() {
+  return Math.ceil(Math.random() * 6);
 }
+
+function showMsg(msg){
+  heading.innerText = msg;
+}
+
+function generateRandomImage(image){
+  return `images/dice${image}.png`;
+}
+
+let randomNumber1 = generateRandomNumber();
+let randomImage = generateRandomImage(randomNumber1);
+img1.setAttribute("src", randomImage);
+
+let randomNumber2 = generateRandomNumber();
+let randomImage2 = generateRandomImage(randomNumber2);
+img2.setAttribute("src", randomImage2);
+
+
+if (randomNumber1 > randomNumber2) {
+  showMsg ("🚩 Player 1 wins!");
+} else if (randomNumber1 === randomNumber2) {
+  showMsg ("Draw!");
+} else {
+  showMsg ("Player 2 wins! 🚩");
+}
+
+reloadBtn.addEventListener('click', function clickRefreshHandler() {
+
+  
+let randomNumber1 = generateRandomNumber();
+let randomImage = generateRandomImage(randomNumber1);
+img1.setAttribute("src", randomImage);
+
+let randomNumber2 = generateRandomNumber();
+let randomImage2 = generateRandomImage(randomNumber2);
+img2.setAttribute("src", randomImage2);
+
+
+if (randomNumber1 > randomNumber2) {
+  showMsg ("🚩 Player 1 wins!");
+} else if (randomNumber1 === randomNumber2) {
+  showMsg ("Draw!");
+} else {
+  showMsg ("Player 2 wins! 🚩");
+}
+
+});
